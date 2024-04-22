15ae866d65f2b5c9d079f9526cbcc2f3a7db077a
thangavele <thangavele@syncfusion.com>
2024-04-22 11:43:03 +0530

    Committed the sample for Blazor Mail merge sample

---
+++ Model/Invoice.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace RichTextEditorMailMerge.Model
+{
+    public class Invoice
+    {
+        public string? InvoiceNumber { get; set; }
+        public string? IssueDate { get; set; }
+        public string? DueDate { get; set; }
+
+        public UserDetails? CompanyDetails { get; set; }
+        public UserDetails? CustomerDetails { get; set; }
+
+    }
+    public class UserDetails
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
